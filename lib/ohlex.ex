cdd291edfbe07d633852507be81802028de445fe
alde103 <aldebaran_103@hotmail.com>
2022-04-04 10:48:58 -0500

    initial commit

---
+++ lib/ohlex.ex
@@ -0,0 +1,18 @@
+defmodule Ohlex do
+  @moduledoc """
+  Documentation for `Ohlex`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Ohlex.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/ohlex_test.exs
@@ -0,0 +1,8 @@
+defmodule OhlexTest do
+  use ExUnit.Case
+  doctest Ohlex
+
+  test "greets the world" do
+    assert Ohlex.hello() == :world
+  end
+end
