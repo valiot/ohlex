14ca522fb6fa3e5fe8eca523b52a7cb86e3cac8f
alde103 <aldebaran_103@hotmail.com>
2022-04-05 00:55:22 -0500

    Build Omron Frame function added

--- lib/host_link/omron.ex
+++ lib/host_link/omron.ex
@@ -1,9 +1,76 @@
 defmodule Ohlex.HostLink.Omron do
   @moduledoc """
   Host Link protocol stack of Omron PLCs.
+  Note: maximum length of command string should be <=80 characters
   """
-  defmodule Ohlex.HostLink.Omron.Frame do
-    defstruct [:cmd_type, :len, :address, :device_id]
+
+  alias Ohlex.HostLink.Omron.Frame
+
+  @headers %{read_ir: "RR", write_ir: "WR", read_dm: "RD", write_dm: "WD"}
+
+  defmodule Frame do
+    @supported_headers [:read_ir, :write_ir, :read_dm, :write_dm, "RR", "WR", "RD", "WD"]
+
+    defstruct [:header, :len, :address, :device_id, :frame, :args]
+
+    def new(cmd_args) do
+      with  {:ok, header} <- Keyword.fetch(cmd_args, :header),
+            true <- header in @supported_headers,
+            {:ok, address} <- Keyword.fetch(cmd_args, :address),
+            true <- is_integer(address),
+            {:ok, device_id} <- Keyword.fetch(cmd_args, :device_id),
+            true <- is_integer(device_id),
+            true <- device_id < 256,
+            args <- Keyword.get(cmd_args, :args, []),
+            true <- is_list(args),
+            len <- Keyword.get(cmd_args, :len, 0),
+            true <- is_integer(len) do
+        {:ok, %__MODULE__{header: header, len: len, address: address, device_id: device_id, frame: "", args: args}}
+      else
+        _error ->
+          {:error, :einval}
+      end
+    end
+  end
+
+  def build_frame(cmd_args) do
+    with  {:ok, frame_struct} <- Frame.new(cmd_args) do
+      complete_frame =
+        frame_struct
+        |> add_initial_char()
+        |> add_device_id()
+        |> add_header()
+        |> add_address()
+        |> add_args()
+        |> add_data_count()
+        |> add_fcs()
+        |> add_termination()
+
+      {:ok, complete_frame.frame}
+    else
+      error ->
+        error
+    end
+  end
+
+  def parse(omron_frame) do
+    with  {:ok, frame_struct} <- Frame.new(cmd_args) do
+      complete_frame =
+        frame_struct
+        |> add_initial_char()
+        |> add_device_id()
+        |> add_header()
+        |> add_address()
+        |> add_args()
+        |> add_data_count()
+        |> add_fcs()
+        |> add_termination()
+
+      {:ok, complete_frame.frame}
+    else
+      error ->
+        error
+    end
   end
 
 
@@ -28,4 +95,63 @@ defmodule Ohlex.HostLink.Omron do
         false
     end
   end
+
+  defp add_initial_char(%{frame: frame} = frame_struct), do: %{frame_struct | frame: frame <> "@"}
+
+  defp add_device_id(%{frame: frame, device_id: device_id} = frame_struct) do
+    with  device_id_str <- Integer.to_string(device_id, 16),
+          device_id_str <- force_two_digits(device_id_str, device_id) do
+      %{frame_struct | frame: frame <> device_id_str}
+    end
+  end
+  defp force_two_digits(device_id_str, device_id) when device_id > 15, do: device_id_str
+  defp force_two_digits(device_id_str, _device_id), do: "0" <> device_id_str
+
+  defp add_header(%{header: header, frame: frame} = frame_struct) when is_atom(header),
+    do: %{frame_struct | frame: frame <> @headers[header]}
+  defp add_header(%{header: header, frame: frame} = frame_struct),
+    do: %{frame_struct | frame: frame <> header}
+
+  defp add_address(%{frame: frame, address: address} = frame_struct) do
+    with  address_str <- Integer.to_string(address),
+          address_str <- force_four_digits(address_str) do
+      %{frame_struct | frame: frame <> address_str}
+    end
+  end
+
+  defp force_four_digits(<<_n_1, _n_2, _n_3, _n_4>> = address), do: address
+  defp force_four_digits(number_str) do
+    str_len = String.length(number_str)
+    for _index <- 0..(3-str_len), reduce: number_str do
+      acc ->
+        "0" <> acc
+    end
+  end
+
+  defp add_args(%{args: []} = frame_struct), do: frame_struct
+  defp add_args(%{frame: frame, args: args} = frame_struct) do
+    args_str =
+      for arg <- args, reduce: "" do
+        acc ->
+          with  arg_str <- Integer.to_string(arg, 16),
+                arg_str <- force_four_digits(arg_str) do
+            acc <> arg_str
+          end
+      end
+    %{frame_struct | frame: frame <> args_str}
+  end
+
+  defp add_data_count(%{len: 0} = frame_struct), do: frame_struct
+  defp add_data_count(%{frame: frame, len: len} = frame_struct) do
+    with  len_str <- Integer.to_string(len, 16),
+          len_str <- force_four_digits(len_str) do
+      %{frame_struct | frame: frame <> len_str}
+    end
+  end
+
+  defp add_fcs(%{frame: frame} = frame_struct),
+    do: %{frame_struct | frame: frame <> compute_fcs(frame)}
+
+  defp add_termination(%{frame: frame} = frame_struct),
+    do: %{frame_struct | frame: frame <> "*\r"}
 end
--- test/host_link/omnron_test.exs
+++ test/host_link/omnron_test.exs
@@ -2,7 +2,6 @@ defmodule Ohlex.HostLinkOmron.Test do
   use ExUnit.Case
   alias Ohlex.HostLink.Omron
 
-
   test "compute_fcs/1" do
     assert Omron.compute_fcs("@04RD02070001") == "56"
     assert Omron.compute_fcs("@04RD000020") == "50"
@@ -19,6 +18,53 @@ defmodule Ohlex.HostLinkOmron.Test do
 
     test "Validates a frame without a termination" do
       refute Omron.is_frame_valid?("@04RD0207000156")
+    end
+  end
+
+  describe "build_frame/1" do
+    test "Valid Read IR Registers cmd" do
+      rr_cmd = [header: :read_ir, len: 7, address: 128, device_id: 1]
+      assert Omron.build_frame(rr_cmd) == {:ok, "@01RR012800074D*\r"}
+    end
+
+    test "Valid WRITE IR Registers cmd" do
+      wr_cmd = [header: :write_ir, address: 256, args: [1, 18, 291, 4660], device_id: 1]
+      assert Omron.build_frame(wr_cmd) == {:ok, "@01WR0256000100120123123443*\r"}
+    end
+
+    test "Valid Read Data Memory DM cmd" do
+      rd_cmd = [header: :read_dm, len: 19, address: 112, device_id: 1]
+      assert Omron.build_frame(rd_cmd) == {:ok, "@01RD0112001357*\r"}
+    end
+
+    test "Valid WRITE Data Memory DM cmd" do
+      wr_cmd = [header: :write_dm, address: 4321, args: [4660, 291, 18, 1], device_id: 1]
+      assert Omron.build_frame(wr_cmd) == {:ok, "@01WD4321123401230012000150*\r"}
+    end
+
+    test "Invalid Header" do
+      cmd = [header: :invalid_header, address: 4321, args: [4660, 291, 18, 1], device_id: 1]
+      assert Omron.build_frame(cmd) == {:error, :einval}
+    end
+
+    test "Invalid Address" do
+      cmd = [header: :write_dm, address: "4321", args: [4660, 291, 18, 1], device_id: 1]
+      assert Omron.build_frame(cmd) == {:error, :einval}
+    end
+
+    test "Invalid Args" do
+      cmd = [header: :write_dm, address: 4321, args: {4660, 291, 18, 1}, device_id: 1]
+      assert Omron.build_frame(cmd) == {:error, :einval}
+    end
+
+    test "Invalid Device ID" do
+      cmd = [header: :write_dm, address: 4321, args: [4660, 291, 18, 1], device_id: "1"]
+      assert Omron.build_frame(cmd) == {:error, :einval}
+    end
+
+    test "Invalid lenght" do
+      cmd = [header: :read_dm, len: "19", address: 112, device_id: 1]
+      assert Omron.build_frame(cmd) == {:error, :einval}
     end
   end
 end
